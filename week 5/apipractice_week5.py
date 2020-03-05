9e8e5cc5144a3fd903d188a22bf31a6f6d247d03
412 Food Rescue <412foodrescue@412s-macbook.ccac.org>
2020-03-04 20:12:34 -0500

    Here are the changes
    Thank you for your help.

--- week 5/apipractice_week5.py
+++ week 5/apipractice_week5.py
@@ -1,12 +1,6 @@
+  
+import requests, json, csv
 
-def readFile():
-    apiURL='https://api.donorschoose.org/common/json_feed.html?snacks-for-better-testing/4653507/&APIKey=DONORSCHOOSE&showSynopsis=true&max=50'
-    #result = getAverage(apiURL)
-    #print(result)
-    trans(apiURL)
-    
-    
-import requests, json
 def getAverage(url):
     rep=requests.get(url)
     runningTotal = 0
@@ -20,8 +14,12 @@ def getAverage(url):
         avg=runningTotal/ len(proposalList)
         return avg
 
-
-import csv
+def readFile():
+    apiURL='https://api.donorschoose.org/common/json_feed.html?snacks-for-better-testing/4653507/&APIKey=DONORSCHOOSE&showSynopsis=true&max=50'
+    result = getAverage(apiURL)
+    print(result)
+    trans(apiURL)
+    
 def trans(url):
     rep=requests.get(url)
     apiDict=json.loads(rep.text)
@@ -31,19 +29,17 @@ def trans(url):
             csv_writer=csv.writer(csvfile)
             csv_writer.writerow(keywords)
     
-    for p in csv_proposals:
-        csv_values=[]
-        for keywords in open('keywords.txt', newline='\n'):
-            for key in keywords:
-                csv_values+=p[key]
-            
-        
-                with open ('list.csv', 'a+', newline='') as csvfile:
-                    csv_writer=csv.writer(csvfile)
-                    csv_writer.writerow(csv_values)  
-readFile()       
+    #for p in csv_proposals:
+        #csv_values=[]
+        #for keywords in open('keywords.txt', newline='\n'):
+            #for key in keywords:
+                #csv_values+= p[key]
+#Thank you for your help, Jinrong!
 
-
+if __name__ == '__main__':
+    readFile()
+    trans('https://api.donorschoose.org/common/json_feed.html?snacks-for-better-testing/4653507/&APIKey=DONORSCHOOSE&showSynopsis=true&max=50')
+    getAverage('https://api.donorschoose.org/common/json_feed.html?snacks-for-better-testing/4653507/&APIKey=DONORSCHOOSE&showSynopsis=true&max=50')
 
    
 
